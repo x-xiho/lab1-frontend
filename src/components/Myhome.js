df8d6dbfbd3e375ca3da06b67589bb2a1cebd350
x-xiho <x_xiho3o@naver.com>
2023-11-09 18:43:38 +0900

    네비게이션 임시 주석처리

--- src/components/Myhome.js
+++ src/components/Myhome.js
@@ -1,11 +1,11 @@
 // import React, { useEffect } from 'react'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 function Myhome() {
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   //로그인 안했으면 로그인 페이지로 강제 이동
   // useEffect(() => {
   //   const userLogin = localStorage.getItem('name')
