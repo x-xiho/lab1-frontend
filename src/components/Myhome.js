369a84bdbe673623373b8e338c7c93247ac26bc9
x-xiho <x_xiho3o@naver.com>
2023-11-09 18:43:07 +0900

    import useEffect 임시 주석처리

--- src/components/Myhome.js
+++ src/components/Myhome.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
