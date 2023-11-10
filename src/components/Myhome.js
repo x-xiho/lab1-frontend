75699aaae6849ecd816a22f01e0aab583b82cb9b
x-xiho <x_xiho3o@naver.com>
2023-11-10 14:45:49 +0900

    myhome 로그인 필수 주석처리

--- src/components/Myhome.js
+++ src/components/Myhome.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 function Myhome() {
 
-  const navigate = useNavigate();
-  //로그인 안했으면 로그인 페이지로 강제 이동
-  useEffect(() => {
-    const userLogin = localStorage.getItem('name')
+  // import React, { useEffect } from 'react'
+  // const navigate = useNavigate();
+  // //로그인 안했으면 로그인 페이지로 강제 이동
+  // useEffect(() => {
+  //   const userLogin = localStorage.getItem('name')
 
-    if (userLogin === null) {
-      // window.location.replace('/login')
-      navigate('/login');
-    }
-  })
+  //   if (userLogin === null) {
+  //     // window.location.replace('/login')
+  //     navigate('/login');
+  //   }
+  // })
 
 
   return (
