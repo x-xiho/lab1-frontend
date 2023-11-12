eeec3b4fb717c8b98b2b157e3af9617f61fd9e64
x-xiho <x_xiho3o@naver.com>
2023-11-12 18:35:05 +0900

    코드 동기화

--- src/components/Myhome.js
+++ src/components/Myhome.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Myhome() {
 
-  // import React, { useEffect } from 'react'
-  // const navigate = useNavigate();
-  // //로그인 안했으면 로그인 페이지로 강제 이동
-  // useEffect(() => {
-  //   const userLogin = localStorage.getItem('name')
+  const navigate = useNavigate();
+  //로그인 안했으면 로그인 페이지로 강제 이동
+  useEffect(() => {
+    const userLogin = localStorage.getItem('name')
 
-  //   if (userLogin === null) {
-  //     // window.location.replace('/login')
-  //     navigate('/login');
-  //   }
-  // })
+    if (userLogin === null) {
+      // window.location.replace('/login')
+      navigate('/login');
+    }
+  })
 
 
   return (
--- src/components/pages/PowerBI.js
+++ src/components/pages/PowerBI.js
@@ -4,15 +4,16 @@ import { models } from 'powerbi-client';
 
 // 도봉구 샘플
 function PowerBI() {
+
+
   return (
     <div>
       <PowerBIEmbed
         embedConfig={{
           type: 'report', // 보고서 타입
           id: 'dbb8d222-164d-42d3-88a2-785abcc3dc57',
-          // URL 수정 요함
-          embedUrl:"https://app.powerbi.com/reportEmbed?reportId=dbb8d222-164d-42d3-88a2-785abcc3dc57&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUVBU1QtQVNJQS1BLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7InVzYWdlTWV0cmljc1ZOZXh0Ijp0cnVlfX0%3d",
-          accessToken:"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSIsImtpZCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZmQwOWIyYmMtOTIyMC00YzZiLTgzNzItMjIwYjdiZDUxODE5LyIsImlhdCI6MTY5OTU5NjgzMywibmJmIjoxNjk5NTk2ODMzLCJleHAiOjE2OTk2MDEyODksImFjY3QiOjAsImFjciI6IjEiLCJhZ2VHcm91cCI6IjQiLCJhaW8iOiJBVFFBeS84VkFBQUFIVGpiVUJDMVlKdE1Dd29OclYrMGd2czRxZkhSLzB4UG1SWkxlTzR5TUJnWW5JVnNEcXY4RHUrUFZXWFEvMWE4IiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImZhbWlseV9uYW1lIjoiS2ltIiwiZ2l2ZW5fbmFtZSI6IkppaG8iLCJpcGFkZHIiOiIyMjEuMTU0LjI0LjgxIiwibmFtZSI6IktpbUppaG8iLCJvaWQiOiIxYzc0NjdhNC0xYjMyLTRiMjUtYTZiMS0wNjUxNTg4ZTUwM2EiLCJwdWlkIjoiMTAwMzIwMDA0OTAzNkM2RiIsInJoIjoiMC5BWEVBdkxJSl9TQ1NhMHlEY2lJTGU5VVlHUWtBQUFBQUFBQUF3QUFBQUFBQUFBQnhBTVUuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoicFZ3N2VHcW1hb2NUcVl5X0ZRdGFhZUlnb3dDOHNZSlUyb0RQdDIwVXVWayIsInRpZCI6ImZkMDliMmJjLTkyMjAtNGM2Yi04MzcyLTIyMGI3YmQ1MTgxOSIsInVuaXF1ZV9uYW1lIjoia29uZzg4QGR1a3N1bmcuYWMua3IiLCJ1cG4iOiJrb25nODhAZHVrc3VuZy5hYy5rciIsInV0aSI6Ijllakp3VU85U1VTbEtkUjktUUVVQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfcGwiOiJrby1LUiJ9.evuDhprSiZ3onsFkI5eCgVSJmL_M4ObcXd9QFfBhK3O7g2f-q5PByZabTuf-pZoO5xuF8JJq7WPkryI5fE_I-tx1l-hBinkGgDxZWaifJXcMlSr9QXEeO4GWQinsUVKrq09yeoXS7Y45CvEhNeSvV20t8elMGpEK9wn29Aq89UuVZBH_wdcnFMEgOCLkfEkr1Ow6spaRXdLzKdd5fy0_cM2jyIBksgKR-isjo-tYHQgPn0Mx1Mb6-RRoRsj_vn7EOq6yXMITDOApFs7xTwrTq445KDyb3Lz4CTv1W2wYCxUGbkLKhWELwE4fGIBZBOzu9oNunHXaZuHU3nEtKacrQA",
+          embedUrl: "https://app.powerbi.com/reportEmbed?reportId=dbb8d222-164d-42d3-88a2-785abcc3dc57&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUVBU1QtQVNJQS1BLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7InVzYWdlTWV0cmljc1ZOZXh0Ijp0cnVlfX0%3d",
+          accessToken:"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSIsImtpZCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZmQwOWIyYmMtOTIyMC00YzZiLTgzNzItMjIwYjdiZDUxODE5LyIsImlhdCI6MTY5OTc3ODYxNiwibmJmIjoxNjk5Nzc4NjE2LCJleHAiOjE2OTk3ODQxOTMsImFjY3QiOjAsImFjciI6IjEiLCJhZ2VHcm91cCI6IjQiLCJhaW8iOiJBVFFBeS84VkFBQUE4YmdFaWJRc210dEUySnV0SkhaTHhPcVV3dTlIa3VhMjgrckhRWXMzTUFTSUhLMVdxM0xnT0RTSVh3WUcwVHZmIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImZhbWlseV9uYW1lIjoiS2ltIiwiZ2l2ZW5fbmFtZSI6IkppaG8iLCJpcGFkZHIiOiIyMjEuMTU0LjI0LjgxIiwibmFtZSI6IktpbUppaG8iLCJvaWQiOiIxYzc0NjdhNC0xYjMyLTRiMjUtYTZiMS0wNjUxNTg4ZTUwM2EiLCJwdWlkIjoiMTAwMzIwMDA0OTAzNkM2RiIsInJoIjoiMC5BWEVBdkxJSl9TQ1NhMHlEY2lJTGU5VVlHUWtBQUFBQUFBQUF3QUFBQUFBQUFBQnhBTVUuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoicFZ3N2VHcW1hb2NUcVl5X0ZRdGFhZUlnb3dDOHNZSlUyb0RQdDIwVXVWayIsInRpZCI6ImZkMDliMmJjLTkyMjAtNGM2Yi04MzcyLTIyMGI3YmQ1MTgxOSIsInVuaXF1ZV9uYW1lIjoia29uZzg4QGR1a3N1bmcuYWMua3IiLCJ1cG4iOiJrb25nODhAZHVrc3VuZy5hYy5rciIsInV0aSI6Il9JUG9qYnFGNjB5NTQ3VjRtd01pQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfcGwiOiJrby1LUiJ9.PGwEFuo8RQv005TqrORB7-dmdxN5SAUCuoP2eUpm35VV2yC6oWZpGbs93N8TSIzENkMv7FBSuml1D5LRyoz9Aj_v5ddH_oPkoODsnZItcqB89yOjBTHiuaB-akuzxIK-aXkjNy6KbZwpsu4EBGpAtKecZPQkMShnD3eGVZERXJXcRLYAXS2yiJ6PSI05Sy6-t-3R9Lp_SOtNtDeQnr-OBINwxbVvMhRkEsOVwJA18nTt_QXF0xy68v4ldcJWhUpgbFnRloDPjhhSmkXrwhlfKX-8T3TRTL8P2UfK3MyY0Xo_ZYSoUqzALcA-VOIKxHAAnUhcORjinI532VHkWWcYXQ",
           tokenType: models.TokenType.Aad,
           settings: {
             panes: {
@@ -26,23 +27,23 @@ function PowerBI() {
         }}
 
 
-      eventHandlers={
-        new Map([
-          ['loaded', function () { console.log('Report loaded'); }],
-          ['rendered', function () { console.log('Report rendered'); }],
-          ['error', function (event) { console.log(event.detail); }],
-          ['visualClicked', (event) => {
-            const visualData = event.detail;
-            console.log('Visual Clicked:', visualData)
-          }],
-          ['pageChanged', (event) => console.log(event)],
-        ])
-      }
+        eventHandlers={
+          new Map([
+            ['loaded', function () { console.log('Report loaded'); }],
+            ['rendered', function () { console.log('Report rendered'); }],
+            ['error', function (event) { console.log(event.detail); }],
+            ['visualClicked', (event) => {
+              const visualData = event.detail;
+              console.log('Visual Clicked:', visualData)
+            }],
+            ['pageChanged', (event) => console.log(event)],
+          ])
+        }
 
-      cssClassName={"embededReport"}
-      getEmbeddedComponent={(embeddedReport) => {
-        window.report = embeddedReport;
-      }}
+        cssClassName={"embededReport"}
+        getEmbeddedComponent={(embeddedReport) => {
+          window.report = embeddedReport;
+        }}
       />
     </div>
   )
